20938b066653f5212e0ec3cc5a147c3a67881a3c
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2023-09-04 12:11:42 -0300

    Update PlotResultsMultiInt.m

--- ExpsSynth/PlotResultsMultiInt.m
+++ ExpsSynth/PlotResultsMultiInt.m
@@ -1,310 +1,199 @@
-drt = '/media/Datos/joaquinruiz/MissingDatSynth/MultiInt_Simu10';
-%drt = '';
-N = 8000;
+addpath(genpath('C:\Users\Intel\Dropbox\Github'))
+drt = '/media/Datos/joaquinruiz/MissingDatSynth/MultiInt_Noise2';
+
+N = 4000;
+fs = 4000;
+load(fullfile(drt,['Results_MissingDataMultiInt_200_10.mat']))
 ratio = 0.05:0.05:0.2;
 Nl = length(ratio);
+vSNR = [10, 20, Inf];
+ErrorCrit = upper(cellstr(S.Metrics));
+Ei = 1;
+ImpNames = cellstr(S.ImpNames);
+NM = length(ImpNames);
+J = 100;
 
-pval_ip = zeros(1,Nl);
-pval_is = pval_ip;
-pval_sp = pval_ip;
-pval_il = pval_ip;
-pval_sl = pval_ip;
-pval_pl = pval_ip;
-hval_ip = pval_ip;
-hval_is = pval_ip;
-hval_sp = pval_ip;
-hval_il = pval_ip;
-hval_sl = pval_ip;
-hval_pl = pval_ip;
-med_i = pval_ip;
-med_p = pval_ip;
-med_s = pval_ip;
-med_l = pval_ip;
-iqr_i = pval_ip;
-iqr_p = pval_ip;
-iqr_s = pval_ip;
-iqr_l = pval_ip;
+%%
+med_imp = zeros(Nl,NM);med_spl = med_imp;med_pch = med_imp;med_lin = med_imp;
+best_imp = zeros(Nl,J);best_spl = best_imp;best_pch = best_imp; best_lin = best_imp;
+pval_ip = zeros(Nl,NM);pval_is = pval_ip;pval_il = pval_ip;pval_sp = pval_ip;pval_sl = pval_ip;pval_pl = pval_ip;
+hval_ip = zeros(Nl,NM);hval_is = hval_ip;hval_il = hval_ip;hval_sp = hval_ip;hval_sl = hval_ip;hval_pl = hval_ip;
+times_imp = zeros(Nl,NM);times_decomp = times_imp; times_pch = times_imp;
+times_spl = times_imp; times_lin = times_imp;
+err_imp_best = zeros(J,1); err_spl_best = err_imp_best; err_pch_best = err_imp_best;
 
-Matxy = [0.07,0.75,0.25,0.22;
-         0.35,0.75,0.25,0.22;
-         0.63,0.75,0.25,0.22;
-         0.07,0.45,0.25,0.22;
-         0.35,0.45,0.25,0.22;
-         0.63,0.45,0.25,0.22;
-         0.07,0.15,0.25,0.22];
-
-Yastk = [-0.1,-0.3;
-         -0.1,-0.3;
-         -0.1,-0.3;
-         -0.1,-0.3;
-         -0.1,-0.3;
-         -0.1,-0.3;
-         -0.1,-0.3];
-
+med_ib = zeros(Nl,1); med_sb = med_ib; med_pb = med_ib;
+pval_bip = zeros(Nl,1); pval_bis = zeros(Nl,1); pval_bsp = zeros(Nl,1);
+hval_bip = zeros(Nl,1); hval_bis = zeros(Nl,1); hval_bsp = zeros(Nl,1);
 alpha_b = 0.05/3;
-Nsbp = ceil(sqrt(Nl));
 
-Nip = zeros(1,Nl);
-Nis = zeros(1,Nl);
-Nil = zeros(1,Nl);
+YLim(:,:,1) = [-0.013 0.875; -0.07 1.57; -0.07 1.38; -0.12 1.40];
+YLim(:,:,2) = [-0.08 0.64; -0.08 1.08; -0.08 1.23; -0.08 1.4];
+BP = [0.03,0.03,0,0; 0 0 0 0]';
+UP = [0.8, 1.43 ,1.25, 1.27; 0.58 0.98 1.13 1.27]';
+PosBox = [0.11 0.61 0.34 0.33; 0.55 0.61 0.34 0.33; 0.11 0.18 0.34 0.33; 0.55 0.18 0.34 0.33];
+edgec = [0.4 0.4 0.4];
+for k=1:length(vSNR)
+    SNR = vSNR(k);
+    for i=1:Nl
+        L = round(ratio(i)*N);
+        load(fullfile(drt,['Results_MissingDataMultiInt_' num2str(L) '_' num2str(SNR) '.mat']))
 
-for i=1:Nl
-    L = round(ratio(i)*N);
-    load(fullfile(drt,['Results_MissingDataMultiInt_' num2str(L) '_phival1.mat']))
+        err_imp = S.Err_Imp(:,:,Ei);
+        err_spl = S.Err_Spl(:,:,Ei);
+        err_pch = S.Err_Pch(:,:,Ei);
+        err_lin = S.Err_Lin(:,:,Ei);
 
-    [pval_ip(i),hval_ip(i)] = signrank(S.TMSE_Best,S.TMSE_Pch,'alpha',alpha_b);
-    [pval_is(i),hval_is(i)] = signrank(S.TMSE_Best,S.TMSE_Spl,'alpha',alpha_b); 
-    [pval_sp(i),hval_sp(i)] = signrank(S.TMSE_Spl,S.TMSE_Pch,'alpha',alpha_b);
-    [pval_il(i),hval_il(i)] = signrank(S.TMSE_Best,S.TMSE_Lin,'alpha',alpha_b);
-    [pval_sl(i),hval_sl(i)] = signrank(S.TMSE_Spl,S.TMSE_Lin,'alpha',alpha_b);
-    [pval_pl(i),hval_pl(i)] = signrank(S.TMSE_Pch,S.TMSE_Lin,'alpha',alpha_b);
+        err = {err_imp,err_spl,err_pch,err_lin};
+        %figure(i)
+        %boxplotGroup(err,'PrimaryLabels',{'Imp','Spl','Pch','Lin'},'SecondaryLabels',ImpNames)
+        %title(ErroCrit{Ei})
+        %hold off
 
-    Nip(i) = sum(S.TMSE_Best>S.TMSE_Pch);
-    Nis(i) = sum(S.TMSE_Best>S.TMSE_Spl);
-    Nil(i) = sum(S.TMSE_Best>S.TMSE_Lin);
-    med_i(i) = median(S.TMSE_Best);
-    med_p(i) = median(S.TMSE_Pch);
-    med_s(i) = median(S.TMSE_Spl);
-    med_l(i) = median(S.TMSE_Lin);
-    iqr_i(i) = iqr(S.TMSE_Best);
-    iqr_p(i) = iqr(S.TMSE_Pch);
-    iqr_s(i) = iqr(S.TMSE_Spl);
-    iqr_l(i) = iqr(S.TMSE_Lin);
-    NM = size(S.ImpNames,1);
 
-    figure(1)
-    subplot(Nsbp,Nsbp,i)
-    boxplot([S.TMSE_Best,S.TMSE_Spl,S.TMSE_Pch])
-    %title([Type ' Wave-Shape. L = ' num2str(L)], ['H_{ip} = ' num2str(round(hval_ip(i)))])
-    xticklabels({'Best Imp','Spl','Pch'})
-    xlabel([num2str(ratio(i)*100) '% Missing Data'],'FontSize',12)
-    
-    catM = discretize(S.BestM,NM,'categorical',{'TLM','LSE','DMD','GPR','ARF','ARB'});
-    figure(2)
-    subplot(Nsbp,Nsbp,i)
-    histogram(catM,'DisplayOrder','descend')
-    xlabel([num2str(ratio(i)*100) '% Missing Data'],'FontSize',12)
-    %hold on
-    %if pval_is<alpha_b
-    %    plot([1,2],[-0.11,-0.11],'k')
-    %    text(0.97,-0.1,'|','FontSize',12)
-    %    text(1.45,-0.05,'*','FontSize',14)
-    %    text(1.97,-0.1,'|','FontSize',12)
-    %    set(gca,'Position',Matxy(i,:));
-    %end
-    % 
-    %if pval_ip<alpha_b
-    %    plot([1,3],[-0.35,-0.35],'k')
-    %    text(0.97,-0.35,'|','FontSize',12)
-    %    text(1.95,-0.3,'*','FontSize',14)
-    %    text(2.97,-0.35,'|','FontSize',12)
-    %    set(gca,'Position',Matxy(i,:));
-    %end
-    %yl = ylim;
-    %ylim([-0.5,0.9355])
-    %hold off
-    %ylabel('MSE')
+        for j=1:NM
+            [pval_ip(i,j),hval_ip(i,j)] = signrank(err_imp(:,j),err_pch(:,j),'alpha',alpha_b);
+            [pval_is(i,j),hval_is(i,j)] = signrank(err_imp(:,j),err_spl(:,j),'alpha',alpha_b);
+            %[pval_il(i,j),hval_il(i,j)] = signrank(err_imp(:,j),err_lin(:,j),'alpha',alpha_b);
+            [pval_sp(i,j),hval_sp(i,j)] = signrank(err_spl(:,j),err_pch(:,j),'alpha',alpha_b);
+            %[pval_sl(i,j),hval_sl(i,j)] = signrank(err_spl(:,j),err_lin(:,j),'alpha',alpha_b);
+            %[pval_pl(i,j),hval_pl(i,j)] = signrank(err_pch(:,j),err_lin(:,j),'alpha',alpha_b);
 
-    %h = {S.MSE_Best,S.MSE_Spl,S.MSE_Pch};
-    %figure(2)
-    %subplot(3,3,i)
-    %boxplotGroup(h,'PrimaryLabels',{'I','S','P'},'SecondaryLabels',{'I1','I2','I3'})
-    %title([Type ' Wave-Shape. L = ' num2str(L)], ['H_{ip} = ' num2str(round(hval_ip(i)))])
-    
-    %figure(3)
-    %subplot(3,3,i)
-    %histogram(S.BestM)
-    %xlabel(['L = ' num2str(L)])
+        end
+        med_imp(i,:) = median(err_imp,1,'omitnan');
+        med_spl(i,:) = median(err_spl,1,'omitnan');
+        med_pch(i,:) = median(err_pch,1,'omitnan');
+        med_lin(i,:) = median(err_lin,1,'omitnan');
 
-    %figure(4)
-    %subplot(3,3,i)
-    %plot(S.True(1,:))
-    %hold on
-    %plot(S.S_Imp(1,:))
-    %plot(S.S_Pch(1,:))
+        times_imp(i,:) = mean(S.Times_Imp,'omitnan');
+        times_decomp(i,:) = mean(S.Times_Decomp,'omitnan');
+        times_pch(i,:) = mean(S.Times_Pch,'omitnan');
+        times_spl(i,:) = mean(S.Times_Spl,'omitnan');
+        times_lin(i,:) = mean(S.Times_Lin,'omitnan');
 
+        [~,best_imp(i,:)] = min(err_imp,[],2);
+        [~,best_spl(i,:)] = min(err_spl,[],2);
+        [~,best_pch(i,:)] = min(err_pch,[],2);
+        [~,best_lin(i,:)] = min(err_lin,[],2);
+
+        figure(k)
+        subplot(2,2,i)
+        catM = discretize(best_imp(i,:),[1:NM+1],'categorical',ImpNames);
+        histogram(catM,'DisplayOrder','descend')
+        xlabel([num2str(ratio(i)*100) '% Missing Data'],'FontSize',12)
+        %title('Best Initial Imputation')
+
+        for j=1:J
+            err_imp_best(j) = err_imp(j,best_imp(i,j));
+            err_spl_best(j) = err_spl(j,best_imp(i,j));
+            err_pch_best(j) = err_pch(j,best_imp(i,j));
+        end
+
+        med_ib(i) = median(err_imp_best,'omitnan');
+        med_sb(i) = median(err_spl_best,'omitnan');
+        med_pb(i) = median(err_pch_best,'omitnan');
+
+
+        [pval_bip(i),hval_bip(i)] = signrank(err_imp_best,err_pch_best,'alpha',alpha_b);
+        [pval_bis(i),hval_bis(i)] = signrank(err_imp_best,err_spl_best,'alpha',alpha_b);
+        [pval_bsp(i),hval_bsp(i)] = signrank(err_spl_best,err_pch_best,'alpha',alpha_b);
+        Err_Imp_Best(:,i) = err_imp_best;
+        Err_Spl_Best(:,i) = err_spl_best;
+        Err_Pch_Best(:,i) = err_pch_best;
+        figure(k)
+        set(gcf,'Position',[902 284 1019 662])
+        hax(i) = subplot(2,2,i);
+        boxplot([err_imp_best err_spl_best err_pch_best])
+        xticklabels({'BestImp','HaLI[BI](s)','HaLI[BI](p)'})
+        xlabel([num2str(ratio(i)*100) '% Missing Data'],'FontSize',12)
+        hax(i).TickLabelInterpreter = 'tex';
+        set(hax(i),'Position',PosBox(i,:))
+
+        ylabel(ErrorCrit{Ei})
+        %text(1,BP(i,k),'|','Color',edgec)
+        %text(1.95,BP(i,k),'|','Color',edgec)
+        %line([1.01,1.96],[BP(i,k),BP(i,k)],'Color',edgec)
+        %text(1.45,BP(i,k)+0.02*(YLim(i,2,k)-YLim(i,1,k)),'*','FontSize',16)
+
+        %text(2.01,BP(i,k),'|','Color',edgec)
+        %text(2.96,BP(i,k),'|','Color',edgec)
+        %line([2.03,2.97],[BP(i,k),BP(i,k)],'Color',edgec)
+        %text(2.45,BP(i,k)+0.02*(YLim(i,2,k)-YLim(i,1,k)),'*','FontSize',16)
+
+        %text(0.99,UP(i,k),'|','Color',edgec)
+        %text(2.99,UP(i,k),'|','Color',edgec)
+        %line([1.01,3],[UP(i,k),UP(i,k)],'Color',edgec)
+        %text(1.96,UP(i,k)+0.02*(YLim(i,2,k)-YLim(i,1,k)),'*','FontSize',16)
+
+        %ylim(YLim(i,:,k))
+    end
 end
 
-%% 0.05
-ratio = 0.05;
+%%
+drt_nl  = '/media/Datos/joaquinruiz/MissingDatSynth/MultiInt_Simu18';
+ratio = 0.2;
 L = round(ratio*N);
-load(fullfile(drt,['Results_MissingDataMultiInt_' num2str(L) '_phival1.mat']))
-[~,i] = max(S.TMSE_Best-S.TMSE_Pch);
-sc = S.True(i,:);
-s_imp = S.S_Imp(i,:);
-s_pch = S.S_Pch(i,:);
+load(fullfile(drt_nl,['Results_MissingDataMultiInt_' num2str(L) '_phival1.mat']))
+i=1;
+sc = S.S_MS(i,:);
 
-r = (sc-s_imp~=0);
+s1_ms = sc;
 
-sti = find(diff(r)==1)+1;
-edi = find(diff(r)==-1);
+fmax = 0.5;
+sigma = 5e-5;
+F1ms = STFT_Gauss(s1_ms,length(s1_ms),sigma,fmax);
 
-s_ms = sc;
-s_ms(r==1) = 0;
-
-fmax = 0.25;
-sigma = 1e-4;
-Fms = STFT_Gauss(s_ms,length(s_ms),sigma,fmax);
-
-fs = 4000;
-N = 8000;
 t = 0:1/fs:(N-1)/fs;
 f = 0:fs/N:fs*fmax-fs/N;
 
-figure(5)
-subplot(3,3,1)
-plot(t,s_ms)
+figure(3)
+set(gcf,'Position',[603 101 1026 821])
+subplot(6,1,1)
+plot(t,s1_ms)
 set(gca,'FontSize',12)
-ylabel('Amplitude [A.U.]')
-title('Missing Rate 5%')
-subplot(3,3,[4,7])
-spectro(Fms,t,f)
+%title('Missing Rate 20%')
+text(0,3.5,'Noiseless','FontSize',14,'Interpreter','latex')
+set(gca,'Position',[0.13 0.84 0.775 0.0964])
+subplot(6,1,2:3)
+%spectro(F1ms,t,f)
+imagesc(abs(F1ms)), axis xy, colormap(1-gray)
+set(gca,'FontSize',12)
+%xlabel('Time [s]')
+ylabel('Frequency [Hz]')
+ylim([0 400])
+set(gca,'Position',[0.13 0.553 0.775 0.2451])
+    
+%%
+ratio = 0.2;
+SNR = 10;
+L = round(ratio*N);
+load(fullfile(drt,['Results_MissingDataMultiInt_' num2str(L) '_' num2str(SNR) '.mat']))
+i=20;
+
+
+sc = S.S_MS(i,:);
+s2_ms = sc;
+
+fmax = 0.5;
+sigma = 5e-5;
+F2ms = STFT_Gauss(s2_ms,length(s2_ms),sigma,fmax);
+
+t = 0:1/fs:(N-1)/fs;
+f = 0:fs/N:fs*fmax-fs/N;
+
+figure(3)
+subplot(6,1,4)
+plot(t,s2_ms)
+set(gca,'FontSize',12)
+yticks([-4 0 4])
+%title('Missing Rate 20%')
+text(0,5.7,'SNR = 10 dB','FontSize',14,'Interpreter','latex')
+set(gca,'Position',[0.13 0.375 0.775 0.0928])
+subplot(6,1,5:6)
+spectro(F2ms,t,f)
+imagesc(abs(F2ms)), axis xy, colormap(1-gray)
 set(gca,'FontSize',12)
 xlabel('Time [s]')
 ylabel('Frequency [Hz]')
-
-figure(6)
-subplot(3,3,1)
-plot(t(sti(1):edi(1)),sc(sti(1):edi(1)))
-hold on
-plot(t(sti(1):edi(1)),s_imp(sti(1):edi(1)),'*r')
-plot(t(sti(1):edi(1)),s_pch(sti(1):edi(1)),'+k')
-xlim([t(sti(1)) t(edi(1))])
-hold off
-subplot(3,3,2)
-plot(t(sti(2):edi(2)),sc(sti(2):edi(2)))
-hold on
-plot(t(sti(2):edi(2)),s_imp(sti(2):edi(2)),'*r')
-plot(t(sti(2):edi(2)),s_pch(sti(2):edi(2)),'+k')
-xlim([t(sti(2)) t(edi(2))])
-hold off
-subplot(3,3,3)
-plot(t(sti(3):edi(3)),sc(sti(3):edi(3)))
-hold on
-plot(t(sti(3):edi(3)),s_imp(sti(3):edi(3)),'*r')
-plot(t(sti(3):edi(3)),s_pch(sti(3):edi(3)),'+k')
-xlim([t(sti(3)) t(edi(3))])
-hold off
-
-%% 0.1
-ratio = 0.1;
-L = round(ratio*N);
-load(fullfile(drt,['Results_MissingDataMultiInt_' num2str(L) '_phival1.mat']))
-[~,i] = max(S.TMSE_Best-S.TMSE_Pch);
-sc = S.True(i,:);
-s_imp = S.S_Imp(i,:);
-s_pch = S.S_Pch(i,:);
-
-r = (sc-s_imp~=0);
-
-sti = find(diff(r)==1)+1;
-edi = find(diff(r)==-1);
-
-s_ms = sc;
-s_ms(r==1) = 0;
-
-fmax = 0.25;
-sigma = 1e-4;
-Fms = STFT_Gauss(s_ms,length(s_ms),sigma,fmax);
-
-fs = 4000;
-N = 8000;
-t = 0:1/fs:(N-1)/fs;
-f = 0:fs/N:fs*fmax-fs/N;
-
-figure(5)
-subplot(3,3,2)
-plot(t,s_ms)
-set(gca,'FontSize',12)
-title('Missing Rate 10%')
-subplot(3,3,[5,8])
-spectro(Fms,t,f)
-set(gca,'FontSize',12)
-xlabel('Time [s]')
-
-figure(6)
-subplot(3,3,4)
-plot(t(sti(1):edi(1)),sc(sti(1):edi(1)))
-hold on
-plot(t(sti(1):edi(1)),s_imp(sti(1):edi(1)),'*r')
-plot(t(sti(1):edi(1)),s_pch(sti(1):edi(1)),'+k')
-xlim([t(sti(1)) t(edi(1))])
-hold off
-subplot(3,3,5)
-plot(t(sti(2):edi(2)),sc(sti(2):edi(2)))
-hold on
-plot(t(sti(2):edi(2)),s_imp(sti(2):edi(2)),'*r')
-plot(t(sti(2):edi(2)),s_pch(sti(2):edi(2)),'+k')
-xlim([t(sti(2)) t(edi(2))])
-hold off
-subplot(3,3,6)
-plot(t(sti(3):edi(3)),sc(sti(3):edi(3)))
-hold on
-plot(t(sti(3):edi(3)),s_imp(sti(3):edi(3)),'*r')
-plot(t(sti(3):edi(3)),s_pch(sti(3):edi(3)),'+k')
-xlim([t(sti(3)) t(edi(3))])
-hold off
-
-%% 0.2
-
-ratio = 0.2;
-L = round(ratio*N);
-load(fullfile(drt,['Results_MissingDataMultiInt_' num2str(L) '_phival1.mat']))
-[~,i] = max(S.TMSE_Best-S.TMSE_Pch);
-sc = S.True(i,:);
-s_imp = S.S_Imp(i,:);
-s_pch = S.S_Pch(i,:);
-
-r = sc-s_imp~=0;
-
-sti = find(diff(r)==1)+1;
-edi = find(diff(r)==-1);
-
-s_ms = sc;
-s_ms(r==1) = 0;
-
-fmax = 0.25;
-sigma = 1e-4;
-Fms = STFT_Gauss(s_ms,length(s_ms),sigma,fmax);
-
-fs = 4000;
-N = 8000;
-t = 0:1/fs:(N-1)/fs;
-f = 0:fs/N:fs*fmax-fs/N;
-
-figure(5)
-subplot(3,3,3)
-plot(t,s_ms)
-set(gca,'FontSize',12)
-title('Missing Rate 20%')
-subplot(3,3,[6,9])
-spectro(Fms,t,f)
-set(gca,'FontSize',12)
-xlabel('Time [s]')
-
-figure(6)
-subplot(3,3,7)
-plot(t(sti(1):edi(1)),sc(sti(1):edi(1)))
-hold on
-plot(t(sti(1):edi(1)),s_imp(sti(1):edi(1)),'*r')
-plot(t(sti(1):edi(1)),s_pch(sti(1):edi(1)),'+k')
-xlim([t(sti(1)) t(edi(1))])
-hold off
-subplot(3,3,8)
-plot(t(sti(2):edi(2)),sc(sti(2):edi(2)))
-hold on
-plot(t(sti(2):edi(2)),s_imp(sti(2):edi(2)),'*r')
-plot(t(sti(2):edi(2)),s_pch(sti(2):edi(2)),'+k')
-xlim([t(sti(2)) t(edi(2))])
-hold off
-subplot(3,3,9)
-plot(t(sti(3):edi(3)),sc(sti(3):edi(3)))
-hold on
-plot(t(sti(3):edi(3)),s_imp(sti(3):edi(3)),'*r')
-plot(t(sti(3):edi(3)),s_pch(sti(3):edi(3)),'+k')
-xlim([t(sti(3)) t(edi(3))])
-hold off
-
-figure(5)
-set(gcf,'Position',[4 45 1032 917])
-
+ylim([0 400])
+set(gca,'Position',[0.13 0.08 0.775 0.2541])
